3c4d820cdfcf9ccbb8a3304a75a0f20a6a7d5289
Nadson Mendes <nadsonfbm@hotmail.com>
2023-04-18 17:49:51 -0300

    committttt

--- control_panel/control_panel.js
+++ control_panel/control_panel.js
@@ -15,10 +15,61 @@ function fetchComponent(path, elementId) {
     .then((response) => response.text())
     .then((html) => {
       document.getElementById(elementId).innerHTML = html;
+      if (elementId === "sidebar") {
+        addSidebarClickEvents(); // Adicione essa linha
+      }
     })
     .catch((error) => {
       console.warn(`Erro ao carregar o componente ${elementId}:`, error);
     });
 }
 
+function toggleSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.classList.toggle("hidden");
+}
 
+function addSidebarClickEvents() {
+  const sidebarLinks = document.querySelectorAll(".sidebar a");
+
+  sidebarLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      const parentLi = event.target.parentElement;
+
+      if (parentLi.querySelector("ul")) {
+        toggleSubMenu(parentLi);
+      } else {
+        loadContentInMain(event.target.href);
+        event.preventDefault();
+        closeAllSubMenusExcept(parentLi);
+      }
+    });
+  });
+}
+
+function toggleSubMenu(parentLi) {
+  const submenu = parentLi.querySelector("ul");
+  submenu.classList.toggle("open");
+}
+
+function loadContentInMain(url) {
+  fetch(url)
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById("main").innerHTML = html;
+    })
+    .catch((error) => {
+      console.warn("Erro ao carregar o conteúdo:", error);
+    });
+}
+
+function closeAllSubMenusExcept(parentLi) {
+  const allSubMenus = document.querySelectorAll(".sidebar ul ul");
+  allSubMenus.forEach((submenu) => {
+    if (!parentLi.contains(submenu)) {
+      submenu.classList.remove("open");
+    }
+  });
+}
+
+document.getElementById("toggle-sidebar").addEventListener("click", toggleSidebar);
