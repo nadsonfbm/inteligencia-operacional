00fc73305875b9d4d5fb79ca5bc431b5d085c663
Nadson Mendes <nadsonfbm@hotmail.com>
2023-04-19 13:55:19 -0300

    commit 9

--- control_panel/control_panel.js
+++ control_panel/control_panel.js
@@ -1,77 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  loadHTMLComponents();
-
-  // Outras funções que você deseja executar quando o DOM estiver totalmente carregado
-});
-
-function loadHTMLComponents() {
-  fetchComponent("../control_panel/header/header.html", "header");
-  fetchComponent("../control_panel/sidebar/sidebar.html", "sidebar");
-  fetchComponent("../control_panel/footer/footer.html", "footer");
-    // Adicione esta linha após carregar os componentes
-    initializeMenuItems();
-}
-
-function fetchComponent(path, elementId) {
-  fetch(path)
-    .then((response) => response.text())
-    .then((html) => {
-      document.getElementById(elementId).innerHTML = html;
-      if (elementId === "sidebar") {
-        addSidebarClickEvents(); // Adicione essa linha
-      }
-    })
-    .catch((error) => {
-      console.warn(`Erro ao carregar o componente ${elementId}:`, error);
-    });
-}
-
-function toggleSidebar() {
-  const sidebar = document.querySelector(".sidebar");
-  sidebar.classList.toggle("hidden");
-}
-
-function addSidebarClickEvents() {
-  const sidebarLinks = document.querySelectorAll(".sidebar a");
-
-  sidebarLinks.forEach((link) => {
-    link.addEventListener("click", (event) => {
-      const parentLi = event.target.parentElement;
-
-      if (parentLi.querySelector("ul")) {
-        toggleSubMenu(parentLi);
-      } else {
-        loadContentInMain(event.target.href);
-        event.preventDefault();
-        closeAllSubMenusExcept(parentLi);
-      }
-    });
-  });
-}
-
-function toggleSubMenu(parentLi) {
-  const submenu = parentLi.querySelector("ul");
-  submenu.classList.toggle("open");
-}
-
-function loadContentInMain(url) {
+// Carrega o conteúdo de um arquivo HTML usando fetch() e insere na seção correspondente
+function loadHTML(url, elementId) {
   fetch(url)
-    .then((response) => response.text())
-    .then((html) => {
-      document.getElementById("main").innerHTML = html;
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById(elementId).innerHTML = html;
     })
-    .catch((error) => {
-      console.warn("Erro ao carregar o conteúdo:", error);
+    .catch(error => {
+      console.error('Erro ao carregar ' + url, error);
     });
 }
 
-function closeAllSubMenusExcept(parentLi) {
-  const allSubMenus = document.querySelectorAll(".sidebar ul ul");
-  allSubMenus.forEach((submenu) => {
-    if (!parentLi.contains(submenu)) {
-      submenu.classList.remove("open");
-    }
-  });
-}
+// Carrega o conteúdo do header.html e insere na seção com o ID "header"
+loadHTML('./header/header.html', 'header');
 
-document.getElementById("toggle-sidebar").addEventListener("click", toggleSidebar);
+// Carrega o conteúdo do sidebar.html e insere na seção com o ID "sidebar"
+loadHTML('./sidebar/sidebar.html', 'sidebar');
+
+// Carrega o conteúdo do footer.html e insere na seção com o ID "footer"
+loadHTML('./footer/footer.html', 'footer');
+
