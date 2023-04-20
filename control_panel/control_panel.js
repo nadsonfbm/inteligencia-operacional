7311f9ec29d91b43dc0304ace72c9b17b17fc854
Nadson Mendes <nadsonfbm@hotmail.com>
2023-04-20 16:01:01 -0300

    commit control access

--- control_panel/control_panel.js
+++ control_panel/control_panel.js
@@ -23,6 +23,51 @@ loadHTML('./sidebar/sidebar.html', 'sidebar')
 // Carrega o conteúdo do footer.html e insere na seção com o ID "footer"
 loadHTML('./footer/footer.html', 'footer');
 
+// Função para carregar o conteúdo de uma subpágina	na seção com o ID "main" do control_panel.html
+function loadSubpage(url) {
+  fetch(url)
+    .then((response) => response.text())
+    .then((html) => {
+      document.querySelector("#main").innerHTML = html;
+      // Armazena a URL e o conteúdo da subpágina no localStorage
+      localStorage.setItem("subpageURL", url);
+      localStorage.setItem("subpageContent", html);
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+}
+
+// Função para carregar o conteúdo de uma subpágina salva no localStorage
+
+function loadSavedState() {
+  const savedURL = localStorage.getItem("subpageURL");
+  const savedContent = localStorage.getItem("subpageContent");
+
+  if (savedURL && savedContent) {
+    // Atualize a div "main" com o conteúdo salvo
+    document.querySelector("#main").innerHTML = savedContent;
+    
+    // Adicione a classe "active" ao link correspondente na barra lateral
+    const sidebarLinks = document.querySelectorAll(".sidebar a");
+    sidebarLinks.forEach((link) => {
+      if (link.dataset.url === savedURL) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+  }
+}
+
+// Carrega o estado salvo quando o DOM estiver pronto
+document.addEventListener("DOMContentLoaded", () => {
+  loadSavedState();
+});
+
+
+
+
 // Função para resetar a sidebar quando o usuário clicar no link "Painel de Controle"
 function resetSidebar() {
   const submenus = document.querySelectorAll('.sidebar-container ul ul');
@@ -51,6 +96,16 @@ function initializeSidebar() {
       if (!submenu) return;
 
       submenu.classList.toggle('hidden');
+    });
+  });
+
+  // Para carregar o conteúdo de uma subpágina quando o usuário clicar em um link da sidebar
+  const subpageLinks = document.querySelectorAll('.sidebar a[data-url]');
+  subpageLinks.forEach(link => {
+    link.addEventListener('click', event => {
+      event.preventDefault();
+      const subpageUrl = event.target.getAttribute('data-url');
+      loadSubpage(subpageUrl);
     });
   });
 
@@ -104,32 +159,3 @@ function logout() {
   // Redireciona o usuário para a página index.html
   window.location.href = '../index.html';
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  const sidebarLinks = document.querySelectorAll(".sidebar-link");
-  const main = document.getElementById("main");
-
-  sidebarLinks.forEach((link) => {
-    link.addEventListener("click", function (event) {
-      event.preventDefault();
-      const pagePath = link.getAttribute("data-page");
-      loadPage(pagePath);
-    });
-  });
-
-  function loadPage(pagePath) {
-    fetch(pagePath)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Erro ao carregar a página ${pagePath}`);
-        }
-        return response.text();
-      })
-      .then((html) => {
-        main.innerHTML = html;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-});
